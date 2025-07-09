d09d8205c3de5c9fda88b0613ed1252eced518b3
Miikka Lukumies <miikka.lukumies@hotmail.com>
2025-07-10 01:01:10 +0300

    Add support for STM32U545 with custom QEMU

--- main.c
+++ main.c
@@ -1,22 +1,55 @@
 #include <stdint.h>
 
-/* Memory mapped APB UART controller registers, see
+/* MPS-AN505 Memory mapped APB UART controller registers, see
  * https://developer.arm.com/documentation/ddi0479/d/apb-components/apb-uart/programmers-model
  */
 static volatile uint32_t * const UART0_DATA_REG =   (uint32_t*)0x40200000;
-static volatile uint32_t * const UART0_STATE_REG =  (uint32_t*)0x40200004;
 static volatile uint32_t * const UART0_CTRL_REG =   (uint32_t*)0x40200008;
 
 /* CTRL register control bits */
 #define APB_UART_TX_EN      (1 << 0)
 #define APB_UART_RX_EN      (1 << 1)
+#define MPS_UART_ENABLE     (APB_UART_TX_EN)
+
+
+/* STM32U545XX UART registers, see 
+ * https://www.st.com/resource/en/reference_manual/rm0456-stm32u5-series-armbased-32bit-mcus-stmicroelectronics.pdf
+ */
+static volatile uint32_t * const UART0_CR1_REG =   (uint32_t*)0x40013800;
+static volatile uint32_t * const UART0_TDR_REG =   (uint32_t*)0x40013828;
+
+/* CTRL register control bits */
+#define USART_CR1_UE    (uint32_t)(1 << 0)
+#define USART_CR1_TE    (uint32_t)(1 << 3)
+#define STM_UART_ENABLE (USART_CR1_TE | USART_CR1_UE)
+
+#ifdef STM32U545XX
+#define HAS_BOARD
+static volatile uint32_t * const UART_DATA_REGISTER = UART0_TDR_REG;
+static volatile uint32_t * const UART_CONTROL_REGISTER = UART0_CR1_REG;
+#define UART_ENABLE STM_UART_ENABLE
+#endif
+
+#ifdef MPS2AN505
+#define HAS_BOARD
+static volatile uint32_t * const UART_DATA_REGISTER = UART0_DATA_REG;
+static volatile uint32_t * const UART_CONTROL_REGISTER = UART0_CTRL_REG;
+#define UART_ENABLE MPS_UART_ENABLE
+
+#endif
+
+#ifndef HAS_BOARD
+#error "Please select either mps2-an505, stm32u545board as BOARD"
+#endif
+
+
 
 /**
  * @brief Setup UART0 for TX only
  */
 void uart0_setup(void)
 {
-    *UART0_CTRL_REG = (unsigned int)0x1;
+    *UART_CONTROL_REGISTER = UART_ENABLE;
 }
 
 /**
@@ -25,7 +58,7 @@ void uart0_setup(void)
 void uart0_print(const char *s)
 {
     while(*s != '\0') {
-        *UART0_DATA_REG = (unsigned int)(*s++);
+        *UART_DATA_REGISTER = (unsigned int)(*s++);
     }
 }
 
@@ -48,7 +81,7 @@ void qemu_clean_exit(void)
  */
 void main()
 {
-    char *buffer = "Hello World!\n";
+    char * buffer = "Hello World!\n";
 
     uart0_setup();
     uart0_print(buffer);
